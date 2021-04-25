5814de1e22ba8088eb00154e81870c3a44eea768
f5074 <oseongryu@gmail.com>
2021-04-25 19:07:29 +0900

    -

--- 20210425/server.js
+++ 20210425/server.js
@@ -9,4 +9,16 @@ const listen = require("socket.io");
 const io = listen(server);
 io.on("connection", (socket) => {
   // 이 아래 작성합니다
+  socket.on('client send', (data) => {
+    io.emit('server send', data.message);
+  });
+
+  socket.on('join', function (data) {
+    io.emit('join', data.message);
+  });
+  socket.on('leave', function (data) {
+    io.emit('leave', data.message);
+  });
+
+
 });
