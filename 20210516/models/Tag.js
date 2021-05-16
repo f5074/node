09f0c9312743558ee701570439bbb8028f2e1788
f5074 <oseongryu@gmail.com>
2021-05-16 20:20:55 +0900

    20210516

---
+++ 20210516/models/Tag.js
@@ -0,0 +1,26 @@
+const moment = require("moment");
+
+module.exports = (sequelize, DataTypes) => {
+  const Tag = sequelize.define(
+    "Tag",
+    {
+      id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING(200),
+        validate: {
+          len: [0, 50]
+        },
+        allowNull: false
+      }
+    },
+    {
+      tableName: "Tag"
+    }
+  );
+
+  return Tag;
+};
